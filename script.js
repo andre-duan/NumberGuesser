0958bb17ce7e305b100b89df2e2505d789914039
André To <83653109+andre-duan@users.noreply.github.com>
2021-07-21 17:57:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,16 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
-//step 3
 let generateTarget = () => {
   return Math.floor(Math.random() * 10);
 };
 
-//step4
 let compareGuesses = (user, computer, target) => {
   const userG = Math.abs(user - target);
   const computerG = Math.abs(computer - target);
     return userG <= computerG;
 };
 
-//step 5
 let updateScore = winner => {
   if (winner === 'human') {
     humanScore ++;
@@ -24,7 +21,6 @@ let updateScore = winner => {
   }
 };
 
-//step 6
 let advanceRound = () => {
   currentRoundNumber += 1;
 };
